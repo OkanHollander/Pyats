d789605ba25d0634d26321862a3f4792b3826eef
o.hollander <o.hollander1990@gmail.com>
2022-11-02 08:28:36 -0400

    reformat code

--- dq1.py
+++ dq1.py
@@ -6,7 +6,9 @@ from rich import print as rprint
 def get_static_routes(hostname, dev):
     parsed = dev.parse("show ip route")
     get_routes = (Dq(parsed).contains('S').get_values('routes'))
-    rprint(get_routes)
+    num_routes = len(get_routes)
+    rprint(f"{hostname} has {num_routes} static routes in its routing table")
+
 
 
 testbed = load("../testbed.yml")
