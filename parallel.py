13c95f37e18a0e34fc137044c319650be0bea7cd
o.hollander <o.hollander1990@gmail.com>
2022-11-02 07:12:57 -0400

    Added ipdb config

--- parallel.py
+++ parallel.py
@@ -2,13 +2,17 @@ import json
 from rich import print as rprint
 from genie.testbed import load
 from pyats.async_ import pcall
+import ipdb
 
 def get_ospf(dev_name, testbed_value):
     ospf_neighbor = testbed_value.parse("show ip int brief")
     pretty_ospf = json.dumps(ospf_neighbor, indent=2)
     rprint(f"{dev_name}\n{pretty_ospf}\n\n")
+    return ospf_neighbor
 
 
 testbed = load("../testbed.yml")
 testbed.connect(log_stdout = False)
 result = pcall(get_ospf, dev_name=testbed.devices.keys(), testbed_value=testbed.devices.values())
+
+ipdb.set_trace()
