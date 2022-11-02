b067701ead39d29f9bb97767c34cafe077128f9d
o.hollander <o.hollander1990@gmail.com>
2022-11-02 07:06:16 -0400

    extra config for device name

--- parallel.py
+++ parallel.py
@@ -3,12 +3,12 @@ from rich import print as rprint
 from genie.testbed import load
 from pyats.async_ import pcall
 
-def get_ospf(testbed_value):
+def get_ospf(dev_name, testbed_value):
     ospf_neighbor = testbed_value.parse("show ip int brief")
     pretty_ospf = json.dumps(ospf_neighbor, indent=2)
-    rprint(pretty_ospf)
+    rprint(f"{dev_name}\n{pretty_ospf}\n\n")
 
 
 testbed = load("../testbed.yml")
 testbed.connect(log_stdout = False)
-result = pcall(get_ospf, testbed_value=testbed.devices.values())
+result = pcall(get_ospf, dev_name=testbed.devices.keys(), testbed_value=testbed.devices.values())
